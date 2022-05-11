bc47f4c1b57001adcc939f32098af4c6689acdcd
Dario Panici <dpanici@princeton.edu>
2022-05-11 00:29:04 -0400

    final file cleanup

--- debug_plot_quants.m
+++ debug_plot_quants.m
@@ -9,9 +9,8 @@ v_nfp_index=1; % index of v to plot quantities at
 nfp_v_index = v_nfp_index;
 s_index=1; % index of s to plot quantities at (that arent plotted versus s)
 
-
-%% R,Z derivs
-% run('plotting/compare_R_Z_v_derivs.m')
+% uncomment the run statements in order to plot the corresponding
+% quantities for the current data
 
 %% basis vector dot products
 
@@ -28,10 +27,10 @@ s_index=1; % index of s to plot quantities at (that arent plotted versus s)
 
 %% plot ||B||
 
-% run('plotting/compare_mag_B')
+run('plotting/compare_mag_B')
 
 %% Plot B^2 derivatives
-% run('plotting/check_grad_Bsq')
+% run('plotting/check_grad_Bsq') % only works for vacuum equilibria
 
 %% Plot B^u
 
@@ -42,8 +41,8 @@ s_index=1; % index of s to plot quantities at (that arent plotted versus s)
 % run('plotting/compare_BV')
 
 %% Plot J^u, J^v (Units?)
-run('plotting/compare_J')
-% 
+% run('plotting/compare_J')
+
 %% Plot Cyl B
 
 % run('plotting/compare_BR')
@@ -67,7 +66,7 @@ run('plotting/compare_J')
 %% plot metric tensor
 % run('plotting/plot_metric_tensor')
 
-%% compare covariant B derivs I calculate here to ones I get with contravariant B
+%% compare covariant B derivs I numerically calculate here to ones I get analytically with contravariant B
 
 % derivplot(Bs,Bs_u,u)
 % derivplot(Bs,Bs_v,v)
@@ -79,15 +78,3 @@ run('plotting/compare_J')
 % derivplot(Bv,Bv_s,s)
 
 
-%% plot current density
-
-% J = JS.*es + JU.*eu + JV.*ev;
-% figure()
-% contourf(R(:,:,v_nfp_index),Z(:,:,v_nfp_index),abs(J(:,:,v_nfp_index)))
-% colormap jet
-% colorbar
-% caxis([1e5 6e5])
-% xlabel('R (m)')
-% ylabel('Z (m)')
-% axis equal
-% title(sprintf('Current Density I calculate at nfp*phi=%f',v(v_nfp_index)))
